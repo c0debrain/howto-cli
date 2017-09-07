85bcba28d318dec86f487067e0294aa22aa3824d
Mateusz Zieliński <mateusz.mzielinski@gmail.com>
2017-09-07 22:08:35 +0200

    Add readme and a few git commands

---
+++ README.md
@@ -0,0 +1,27 @@
+# Your personal command line assistant
+
+Despite years of experience I have problem to remember all the Unix black magic. I use `git` on daily basis and still strugle with more advanced commands. Introducing `docker` made my experience with terminal even worse.
+
+`howto-cli` comes to the rescue. Ask a question directly in your terminal and get an answer immediately.
+
+![howto-cli demo](https://raw.githubusercontent.com/ziolko/howto-cli/master/docs/animation.gif)
+
+
+# Installation and usage
+`npm i -g howto-cli`
+
+You should now have access to the command `howto`. For the start try a few simple questions:
+- `howto create a file`
+- `howto clone git repo`
+- `howto list active processes`
+
+# Current status
+This project is currently on the proof-of-concept phase. Supported 
+
+# Analytics
+To get as much feedback as possible every question you ask is anonymously sent to my server. This is needed to analyse what questions the knowledge base fails to answer.
+
+Without anonymous usage analytics I am unable to improve the product. If you are concerned about privacy let me know in [this issue](https://github.com/ziolko/howto-cli/issues/1).
+
+# License 
+[MPL-2.0](https://opensource.org/licenses/MPL-2.0)
--- knowledge-base/git.js
+++ knowledge-base/git.js
@@ -1,1 +1,38 @@
 exports.keywords = ['git', 'git', 'git', 'commit', 'push', 'HEAD', 'revision']
+
+// Based on https://www.git-tower.com/blog/git-cheat-sheet
+exports.intends = [
+  // Create
+  {
+    name: 'How to clone an existing git repository',
+    command: 'git clone <repository-url>',
+    patterns: '(clone|copy) (existing|remote)? git? (repo|repository) (in git)?'
+  },
+  {
+    name: 'How to create a new local repository in current directory',
+    command: 'git init',
+    patterns: '(create|init) new? git? (repo|repository) (in git)?'
+  },
+  {
+    name: 'How to list files changed locally in git repository',
+    command: 'git status',
+    patterns: '(see|list|show) (git? changes|changed files|files changed) (in git)?'
+  },
+  // Local changes - TODO
+  // Commit history - TODO
+  // Branches & tags - TODO
+  // Update & publish - TODO
+  // Merge & rebase - TODO
+  // Undo - TODO
+  // Remotes - TODO
+  {
+    name: 'How to list remote git repositories',
+    command: 'git remote -v',
+    patterns: '(list|show|see) git? (remotes|remote repositories) (in git)?'
+  },
+  {
+    name: 'How to add remote git repository',
+    command: 'git remote add <short-name> <repository-url>',
+    patterns: '(add|create) git? remote (repository|repo)? (in git)?'
+  }
+]
